8afb30f7446eae2ccd171c05c94c0ad356fb87c3
Aabid <71463876+aabiddd@users.noreply.github.com>
2022-04-19 18:16:35 +0545

    Add files via upload

---
+++ digitalClock.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    int hour, minute, second;
+    int delay = 1000;
+    printf("Set Time: \n");
+    scanf("%i%i%i", &hour, &minute, &second);
+    if (hour > 12 || minute > 60 || second > 60)
+    {
+        printf("ERROR!! \n");
+        exit(0);
+    }
+    while(1)
+    {
+        second++;
+        if (second > 59)
+        {
+            minute++;
+            second = 0;
+        }
+        if (minute > 59)
+        {
+            hour++;
+            minute = 0;
+        }
+        if (hour > 11)
+        {
+            hour = 0;
+        }
+        printf("\n Clock: ");
+        printf("\n %02i:%02i:%02i", hour, minute, second);
+        Sleep(delay);
+        system("cls");
+    }
+}
